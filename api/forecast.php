587bbef1d9f895157e339cdaa9ef436feec75c3c
ejenegapema <70914345+ejenegapema@users.noreply.github.com>
2026-09-09 13:51:39 +0400

    Update and rename jen.php to forecast.php

--- api/forecast.php
+++ api/forecast.php
@@ -1,26 +1,8 @@
 <?php
-session_start();
-
-// ====== SECURITY CHECKS ======
-if (!isset($_GET['key'])) {
-    http_response_code(403);
-    echo "Access denied. Missing key.";
-    exit;
-}
-if (!isset($_SESSION['download_key']) || $_GET['key'] !== $_SESSION['download_key']) {
-    http_response_code(403);
-    echo "Access denied. Invalid key.";
-    exit;
-}
-if (time() > $_SESSION['key_expires']) {
-    http_response_code(403);
-    echo "Access denied. Key expired.";
-    exit;
-}
 
 // ====== MODEL SHORT NAMES ======
-$MODEL_NAMES = ["icon_global" => "icon", "ecmwf_ifs025" => "ecmwf_ifs", "gfs_global" => "gfs", "meteofrance_arpege_world" => "arpege", 
-                "gfs_graphcast025" => "gfs_graphcast", "ukmo_global_deterministic_10km"=> "ukmet", "gem_global" => "gem", "ecmwf_aifs025_single" => "ecmwf_aifs"];
+$MODEL_NAMES = ["dwd_icon_global" => "icon", "ecmwf_ifs" => "ecmwf_ifs", "ncep_gfs_global" => "gfs", "meteofrance_arpege_world" => "arpege", 
+                "ncep_aigfs025" => "aigfs", "ukmo_global_deterministic_10km"=> "ukmet", "cmc_gem_gdps" => "gem", "ecmwf_aifs025_single" => "ecmwf_aifs"];
 
 // Reverse map: short → full
 $MODEL_ALIASES = array_flip($MODEL_NAMES);
